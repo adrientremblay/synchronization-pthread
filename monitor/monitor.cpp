ef82dad19a4eeddc21f03ffd3109f052bf2cd041
adrientremblay <adrien@adrientremblay.com>
2020-03-23 22:10:47 -0400

    basic working monitor class

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -5,11 +5,27 @@ using namespace std;
 
 #define RUNS 10000000 /* this value is large enough to cause a race condition */
 
-template<class T>
-class Wrapper {
-    
+template <class C>
+class Monitor {
+  private:
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    C slave;
 
-}
+  public:
+    void run(void(C::*func)()) {
+      void(C::*func_ptr)();
+      func_ptr = func;
+      (slave.*func_ptr)();
+    };
+};
+
+class SharedBalance {
+  public:
+    int balance;  
+    void credit(int amount) {balance += amount; std:cout << "hello";};
+    void debit(int amount) {balance -= amount;};
+    void test() {std:cout << "fuck";};
+};
 
 void* count(void* arg);
 
@@ -32,18 +48,21 @@ void* count(void* arg) {
 }
 
 int main() {
-  pthread_t tid1, tid2;
-  long long val1, val2;
-  val1 = 1;
-  val2 = -1; 
+  /* pthread_t tid1, tid2; */
+  /* long long val1, val2; */
+  /* val1 = 1; */
+  /* val2 = -1; */ 
 
-  pthread_create(&tid1, NULL, count, &val1);
-  pthread_create(&tid2, NULL, count, &val2);
+  /* pthread_create(&tid1, NULL, count, &val1); */
+  /* pthread_create(&tid2, NULL, count, &val2); */
   
-  pthread_join(tid1, NULL); /* this is a form a synchronization as the main thread will wait until the two child threads are done executing */
-  pthread_join(tid2, NULL); 
+  /* pthread_join(tid1, NULL); */ 
+  /* pthread_join(tid2, NULL); */ 
 
-  printf("Total is %lld\n", total); /* this will return an incorrect value because of synchronization issues */
+  /* printf("Total is %lld\n", total); */
+  
+  Monitor<SharedBalance> m;
+  m.run(&SharedBalance::test);
 
   return 0;
 }
